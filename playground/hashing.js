7a247b01d999eb83cdb72b500068fee55f27e0c5
Mayank Joshi <mayank@aisle.co>
2017-04-09 01:38:16 +0530

    adding password hash

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,4 +1,5 @@
 const {SHA256} = require('crypto-js');
+const bcrypt = require('bcryptjs');
 
 // var message = 'I am user number 3';
 // var hash = SHA256(message).toString();
@@ -22,13 +23,25 @@ const {SHA256} = require('crypto-js');
 // else {
 //     console.log('data corrupt');
 // }
-const jwt = require('jsonwebtoken');
+// const jwt = require('jsonwebtoken');
+//
+// var data = {
+//     id: 10
+// };
+//
+// var token = jwt.sign(data, 'sign123');
+// console.log(`token: ${token}`);
+// var decoded = jwt.verify(token ,'sign123');
+// console.log(`decoded: ${decoded}`);
 
-var data = {
-    id: 10
-};
+var password = 'mayank';
+bcrypt.genSalt(10, (err, salt) => {
+    bcrypt.hash(password, salt, (err, hash) => {
+        console.log(hash);
+    });
+});
 
-var token = jwt.sign(data, 'sign123');
-console.log(`token: ${token}`);
-var decoded = jwt.verify(token ,'sign123');
-console.log(`decoded: ${decoded}`);
+hashedPassword = '$2a$10$aOwJH1Fgnu6bU8YFiaQra./7I5v1zO.Zaki9MHBHT8crD.pKnXlSy';
+bcrypt.compare(password, hashedPassword, (err, result) => {
+    console.log(result);
+});
